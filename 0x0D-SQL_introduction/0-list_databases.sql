3e6c9310006581d4549223d5e29a4ba30ffe2e51
Bokang06 <bokangshogole45>
2023-12-12 17:28:54 -0800

    SQLTASKS

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,2 @@
--- List all privileges of the MySQL users user_Od_1 and user_Od_2
-SHOW GRAND FOR 'user_Od_1'@'localhost':
-SHOW GRAND FOR 'user_Od_2'@'localhost':
+-- Lists all databases of my MySQL server.
+SHOW DATABASES;
